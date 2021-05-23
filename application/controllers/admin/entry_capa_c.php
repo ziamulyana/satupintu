4afddd25a316b822b44f297e02a78dfaf787ff8e
ziamulyana <ziamulyana13@gmail.com>
2021-05-23 14:37:31 +0700

    q

--- application/controllers/admin/entry_capa_c.php
+++ application/controllers/admin/entry_capa_c.php
@@ -18,4 +18,29 @@ class Entry_capa_c extends MY_Controller
         
         $this->template->load('layouts/admin_template', 'admin/entry_capa_v');
     }
+
+    public function add()
+    {
+        $this->load->model('FeedbackCapa');
+        
+        $post_no_surat = $this->input->post('no_surat');
+        $data = array
+        (
+            'no_surat'          =>$this->input->post('no_surat'),
+            'sarana'            =>$this->input->post('sarana'),
+            'tgl_surat'         =>$this->input->post('tgl_surat'),
+            'tanggal_timeline'  =>$this->input->post('tanggal_timeline')
+        );
+        $checkvalidation = $this->add_timeline_m->checkDuplicate($post_no_surat);
+            if($checkvalidation == true){
+                $this->db->insert('notif',$data);
+                $this->session->set_flashdata('success', 'Data Berhasil Dimasukkan');
+                redirect('admin/entry_capa_c', 'refresh');
+            }else{
+            $this->session->set_flashdata('failed', 'Data Duplikat');
+            redirect('admin/entry_capa_c', 'refresh');
+            }
+     
+        
+    }
 }
