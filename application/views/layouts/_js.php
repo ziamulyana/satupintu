7af4dc3e51112a14be9eb44f5878a817e92a1093
puputk17 <puputk17@outlook.com>
2021-05-27 14:17:23 +0700

    Fix Js Notif, Grafik, Text Area

--- application/views/layouts/_js.php
+++ application/views/layouts/_js.php
@@ -55,9 +55,16 @@
 <script src="<?php echo base_url('assets'); ?>/vendor/jquery/jquery.idle.js"></script>
 <!-- Bootstrap WYSIHTML5 -->
 <script src="<?php echo base_url('assets'); ?>/vendor/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<script src="https://cdn.ckeditor.com/4.4.3/standard/ckeditor.js"></script>
-
-
+<script src="<?php echo base_url('assets'); ?>/vendor/ckeditor/ckeditor.js"></script>
+<script>
+  $(function () {
+    // Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    CKEDITOR.replace('editor1');
+    //bootstrap WYSIHTML5 - text editor
+    $(".textarea").wysihtml5();
+  });
+</script>
 <script>
 	$(document).idle({
 		onIdle: function() {
@@ -131,142 +138,181 @@
 	});
 </script>
 <script>
-	$(function() {
-		/* ChartJS
-		 * -------
-		 * Here we will create a few charts using ChartJS
-		 */
+	$(document).ready(function() {
 
-		//--------------
-		//- AREA CHART -
-		//--------------
+		// ANIMATEDLY DISPLAY THE NOTIFICATION COUNTER.
+		$('#noti_Counter')
+			.css({
+				opacity: 0
+			})
+			.text('<?php echo $this->db->from("notif")->count_all_results(); ?>') // ADD DYNAMIC VALUE (YOU CAN EXTRACT DATA FROM DATABASE OR XML).
+			.css({
+				top: '0px'
+			})
+			.animate({
+				top: '10px',
+				opacity: 1
+			}, 500);
 
-		// Get context with jQuery - using jQuery's .get() method.
-		var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
-		// This will get the first returned node in the jQuery collection.
-		var areaChart = new Chart(areaChartCanvas);
+		$('#noti_Button').click(function() {
 
-		var areaChartData = {
-			labels: ["January", "February", "March", "April", "May", "June", "July", "Agustus", "September", "Oktober", "November", "Desember"],
-			datasets: [{
-					label: "Expired",
-					fillColor: "#dd4b39",
-					strokeColor: "#dd4b39",
-					pointColor: "#dd4b39",
-					pointStrokeColor: "#dd4b39",
-					pointHighlightFill: "#fff",
-					pointHighlightStroke: "#dd4b39",
-					data: [
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',1)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',2)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',3)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',4)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',5)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',6)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',7)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',8)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',9)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',10)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',11)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',12)->where('timeline <=',3)->from("view_notif")->count_all_results(); ?>			
-					]
-				},
-				{
-					label: "Tenggang",
-					fillColor: "#f39c12",
-					strokeColor: "#f39c12",
-					pointColor: "#f39c12",
-					pointStrokeColor: "#f39c12",
-					pointHighlightFill: "#fff",
-					pointHighlightStroke: "#f39c12",
-					data: [
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',1)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',2)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',3)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',4)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',5)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',6)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',7)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',8)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',9)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',10)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',11)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',12)->where('timeline >=',4)->where('timeline <=',7)->from("view_notif")->count_all_results(); ?>
-					]
-				},
-				{
-					label: "Aktif",
-					fillColor: "#00a65a",
-					strokeColor: "#00a65a",
-					pointColor: "#00a65a",
-					pointStrokeColor: "#00a65a",
-					pointHighlightFill: "#fff",
-					pointHighlightStroke: "#00a65a",
-					data: [
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',1)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',2)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',3)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',4)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',5)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',6)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',7)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',8)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',9)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',10)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',11)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>,
-					<?php echo $this->db->where('MONTH(tanggal_timeline)=',12)->where('timeline >',7)->from("view_notif")->count_all_results(); ?>
-					]
+			// TOGGLE (SHOW OR HIDE) NOTIFICATION WINDOW.
+			$('#notifications').fadeToggle('fast', 'linear', function() {
+				if ($('#notifications').is(':hidden')) {
+					$('#noti_Button').css('background-color', '#00537d');
 				}
-			]
-		};
-		//-------------
-		//- BAR CHART -
-		//-------------
-		var barChartCanvas = $("#barChart").get(0).getContext("2d");
-		var barChart = new Chart(barChartCanvas);
-		var barChartData = areaChartData;
-		
-		var barChartOptions = {
-			//Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-			scaleBeginAtZero: true,
-			//Boolean - Whether grid lines are shown across the chart
-			scaleShowGridLines: true,
-			//String - Colour of the grid lines
-			scaleGridLineColor: "rgba(0,0,0,.05)",
-			//Number - Width of the grid lines
-			scaleGridLineWidth: 1,
-			//Boolean - Whether to show horizontal lines (except X axis)
-			scaleShowHorizontalLines: true,
-			//Boolean - Whether to show vertical lines (except Y axis)
-			scaleShowVerticalLines: true,
-			//Boolean - If there is a stroke on each bar
-			barShowStroke: true,
-			//Number - Pixel width of the bar stroke
-			barStrokeWidth: 2,
-			//Number - Spacing between each of the X value sets
-			barValueSpacing: 5,
-			//Number - Spacing between data sets within X values
-			barDatasetSpacing: 1,
-			//String - A legend template
-			legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-			//Boolean - whether to make the chart responsive
-			responsive: true,
-			maintainAspectRatio: true
-		};
+				// CHANGE BACKGROUND COLOR OF THE BUTTON.
+				else $('#noti_Button').css('background-color', '#00537d');
+			});
 
-		
+			$('#noti_Counter').fadeOut('fast'); // HIDE THE COUNTER.
 
-		barChartOptions.datasetFill = false;
-		barChart.Bar(barChartData, barChartOptions);
+			return false;
+		});
+
+		// HIDE NOTIFICATIONS WHEN CLICKED ANYWHERE ON THE PAGE.
+		$(document).click(function() {
+			$('#notifications').hide();
+
+			// CHECK IF NOTIFICATION COUNTER IS HIDDEN.
+			if ($('#noti_Counter').is(':hidden')) {
+				// CHANGE BACKGROUND COLOR OF THE BUTTON.
+				$('#noti_Button').css('background-color', '#00537d');
+			}
+		});
+
+		$('#notifications').click(function() {
+			return true; // DO NOTHING WHEN CONTAINER IS CLICKED.
+		});
 	});
 </script>
 <script>
 	$(function() {
-		// Replace the <textarea id="editor1"> with a CKEditor
-		// instance, using default configuration.
-		
-		//bootstrap WYSIHTML5 - text editor
-		$(".textarea").wysihtml5();
-		CKEDITOR.replace('editor1');
+	/* ChartJS
+	 * -------
+	 * Here we will create a few charts using ChartJS
+	 */
+
+	//--------------
+	//- AREA CHART -
+	//--------------
+
+	// Get context with jQuery - using jQuery's .get() method.
+	var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+	// This will get the first returned node in the jQuery collection.
+	var areaChart = new Chart(areaChartCanvas);
+
+	var areaChartData = {
+		labels: ["January", "February", "March", "April", "May", "June", "July", "Agustus", "September", "Oktober", "November", "Desember"],
+		datasets: [{
+				label: "Expired",
+				fillColor: "#dd4b39",
+				strokeColor: "#dd4b39",
+				pointColor: "#dd4b39",
+				pointStrokeColor: "#dd4b39",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "#dd4b39",
+				data: [
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 1)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 2)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 3)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 4)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 5)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 6)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 7)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 8)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 9)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 10)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 11)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 12)->where('timeline <=', 3)->from("view_notif")->count_all_results(); ?>
+				]
+			},
+			{
+				label: "Tenggang",
+				fillColor: "#f39c12",
+				strokeColor: "#f39c12",
+				pointColor: "#f39c12",
+				pointStrokeColor: "#f39c12",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "#f39c12",
+				data: [
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 1)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 2)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 3)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 4)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 5)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 6)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 7)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 8)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 9)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 10)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 11)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 12)->where('timeline >=', 4)->where('timeline <=', 7)->from("view_notif")->count_all_results(); ?>
+				]
+			},
+			{
+				label: "Aktif",
+				fillColor: "#00a65a",
+				strokeColor: "#00a65a",
+				pointColor: "#00a65a",
+				pointStrokeColor: "#00a65a",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "#00a65a",
+				data: [
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 1)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 2)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 3)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 4)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 5)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 6)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 7)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 8)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 9)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 10)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 11)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>,
+					<?php echo $this->db->where('MONTH(tanggal_timeline)=', 12)->where('timeline >', 7)->from("view_notif")->count_all_results(); ?>
+				]
+			}
+		]
+	};
+	//-------------
+	//- BAR CHART -
+	//-------------
+	var barChartCanvas = $("#barChart").get(0).getContext("2d");
+	var barChart = new Chart(barChartCanvas);
+	var barChartData = areaChartData;
+
+	var barChartOptions = {
+		//Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+		scaleBeginAtZero: true,
+		//Boolean - Whether grid lines are shown across the chart
+		scaleShowGridLines: true,
+		//String - Colour of the grid lines
+		scaleGridLineColor: "rgba(0,0,0,.05)",
+		//Number - Width of the grid lines
+		scaleGridLineWidth: 1,
+		//Boolean - Whether to show horizontal lines (except X axis)
+		scaleShowHorizontalLines: true,
+		//Boolean - Whether to show vertical lines (except Y axis)
+		scaleShowVerticalLines: true,
+		//Boolean - If there is a stroke on each bar
+		barShowStroke: true,
+		//Number - Pixel width of the bar stroke
+		barStrokeWidth: 2,
+		//Number - Spacing between each of the X value sets
+		barValueSpacing: 5,
+		//Number - Spacing between data sets within X values
+		barDatasetSpacing: 1,
+		//String - A legend template
+		legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+		//Boolean - whether to make the chart responsive
+		responsive: true,
+		maintainAspectRatio: true
+	};
+
+
+
+	barChartOptions.datasetFill = false;
+	barChart.Bar(barChartData, barChartOptions);
 	});
 </script>
