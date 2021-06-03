d9cf8e7ba68afc671432672825ba494003dd11f4
endahnasution <79795077+endahnasution@users.noreply.github.com>
2021-06-03 15:11:00 +0700

    feedback editt lage

--- application/models/Feedback_model.php
+++ application/models/Feedback_model.php
@@ -4,38 +4,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Feedback_model extends CI_Model{
 
 
-      function __construct()
-    {
-        parent::__construct();
-    }
+  function __construct()
+  {
+    parent::__construct();
+  }
 
-    public function getFeedback()
-    { 
-      
+  public function getFeedback()
+  { 
+
     $this->db->select('tbl_feedback.idFeedback,tbl_feedback.noSuratFeedback, tbl_sarana.namaSarana, tbl_feedback.tglFeedback,  tbl_feedback.file_feedback, tbl_feedback.closed');
     $this->db->from('tbl_feedback');
     $this->db->join('tbl_peringatan', 'tbl_feedback.idSuratPeringatan = tbl_peringatan.idPeringatan');
-     $this->db->join('tbl_surattl', 'tbl_peringatan.idTl = tbl_surattl.idTl');
-      $this->db->join('tbl_lhk', 'tbl_surattl.idLhk = tbl_lhk.idLhk');
-      $this->db->join('tbl_sarana', 'tbl_lhk.idSarana = tbl_sarana.idSarana');
-    $this->db->order_by("tbl_feedback.closed","asc" );
+    $this->db->join('tbl_surattl', 'tbl_peringatan.idTl = tbl_surattl.idTl');
+    $this->db->join('tbl_lhk', 'tbl_surattl.idLhk = tbl_lhk.idLhk');
+    $this->db->join('tbl_sarana', 'tbl_lhk.idSarana = tbl_sarana.idSarana');
     $query = $this->db->get('');
     return $query;
     
-    }
+  }
 
-    public function getChecklist(){
-         $this->db->select('closed');
-         $this->db->where('closed <', 0);
-         $query = $this->db->get('tbl_feedback');
-         return $query->num_rows();
-    }
+  public function getChecklist(){
+   $this->db->select('*');
+   $this->db->from('tbl_feedback');
+   $this->db->join('tbl_peringatan', 'tbl_feedback.idSuratPeringatan = tbl_peringatan.idPeringatan');
+   $this->db->where('closed <', 0);
+   $query = $this->db->get('');
+   return $query->num_rows();
+ }
 
-    public function updateClosed($id, $editClosed){
-        $this->db->set('closed', $editClosed, FALSE);    
-        $this->db->where('idFeedback', $id);
-        $this->db->update('tbl_feedback'); 
-    }
+ public function updateClosed($id, $editClosed){
+  $this->db->set('closed', $editClosed, FALSE);    
+  $this->db->where('idFeedback', $id);
+  $this->db->update('tbl_feedback'); 
+}
 
 
 
