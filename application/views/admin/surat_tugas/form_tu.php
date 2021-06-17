c500b31d0a4717f36d490fba6043fcabdf9b5d39
AgungBK <agungbudikusuma1@gmail.com>
2021-06-17 11:52:47 +0700

    rev surat tugas

---
+++ application/views/admin/surat_tugas/form_tu.php
@@ -0,0 +1,255 @@
+<section class="content-header">
+  <h1>
+    Buat Surat Tugas Sub Bagian Tata Usaha
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="#">Dashboard</a></li>
+    <li><a href="#">Surat Tugas</a></li>
+  </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+   <div class="col-md-12">
+     <form role="form" action="<?php echo base_url('admin/surat_tugas/surattugas_tu/simpan_surat')?>" method="post">
+      <div class="box box-primary">
+        <div class="box-header with-border">
+          <h3 class="box-title">Form Pembuatan Surat Tugas</h3>
+          <p><span class="wajib">* wajib diisi</span></p>
+        </div>
+
+        <div class="col-md-6">
+          <hr>
+          
+          <!-- nomor surat -->
+          <div class="form-group row">
+            <label for="noSuratTugas" class="col-sm-4 col-form-label">Nomor Surat Tugas<span class="wajib"> *</span></label>
+            <div class="col-sm-12">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                <input type="text" class="form-control" placeholder="T-PW.01.12.9A5." name="noSuratTugas" id="noSuratTugas" required>
+              </div>
+            </div>
+          </div>
+
+          <!-- tanggal surat -->
+          <div class="form-group row">
+            <label for="example-date-input" class="col-sm-5 col-form-label">Tanggal Surat Tugas<span class="wajib"> *</span></label>
+            <div class="col-sm-12"> 
+              <input class="form-control" type="date" name ="tglSurat" id="tglSurat" placeholder="Tanggal" required>
+            </div>
+          </div>
+
+          <!-- alamat tujuan -->
+          <div class="form-group row">
+            <label for="kota" class="col-sm-4 col-form-label">Kota Tujuan<span class="wajib"> *</span></label>
+            <div class="col-sm-12">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-building"></i></span>
+                <select class="form-control" name="kota" id="kota" required>
+                  <option value="" disabled selected>- Pilih Kota -</option>
+                  <option>Kota Batam</option>
+                  <option>Kota Tanjung Pinang</option>
+                  <option>Kabupaten Bintan</option>
+                  <option>Kabupaten Karimun</option>
+                  <option>Kabupaten Anambas</option>
+                  <option>Kabupaten Lingga</option>
+                  <option>Kabupaten Natuna</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <!-- Anggaran -->
+          <div class="form-group row">
+            <label for="anggaran" class="col-sm-4 col-form-label">Anggaran<span class="wajib"> *</span></label>
+            <div class="col-sm-12">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-money"></i></span>
+                <select class="form-control category"  name="idAnggaran" id="idAnggaran" required>
+                <?php
+                foreach ($anggaran as $anggaran) {
+                  echo "<option value=".$anggaran->idAnggaran.">".$anggaran->namaAnggaran."</option>";
+                }
+                ?>
+                <option value="" disabled selected>- Anggaran -</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <!-- Maksud -->
+          <div class="form-group row">
+            <label for="maksud" class="col-sm-4 col-form-label">Maksud<span class="wajib"> *</span></label>
+            <div class="col-sm-12">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-list"></i></span>
+                <textarea class="form-control" name="maksud" id="maksud" placeholder="Enter.." required></textarea>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+        <div class="col-md-6">
+          <hr>
+          
+          <!-- tanggal mulai tugas -->
+          <div class="form-group row">
+            <label for="example-date-input" class="col-sm-5 col-form-label">Tanggal Mulai Tugas<span class="wajib"> *</span></label>
+            <div class="col-sm-12">
+              <input class="form-control" type="date" name ="tglMulai" id="tglMulai" placeholder="Tanggal Mulai Tugas" onclick="return chk_date()" required>
+            </div>
+          </div>
+
+          <!-- tanggal selesai tugas -->
+          <div class="form-group row">
+            <label for="example-date-input" class="col-sm-5 col-form-label">Tanggal Selesai Tugas<span class="wajib"> *</span></label>
+            <div class="col-sm-12">
+              <input class="form-control" type="date" name ="tglSelesai" id="tglSelesai" placeholder="Tanggal Selesai Tugas" onclick="return chk_date()" required>
+            </div>
+          </div>
+
+          <!-- kendaraan -->
+          <div class="form-group row">
+            <label for="kendaraan" class="col-sm-4 col-form-label">Kendaraan<span class="wajib"> *</span></label>
+            <div class="col-sm-12">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-taxi"></i></span>
+                <select class="form-control" name="kendaraan" id="kendaraan" required>
+                  <option value="" disabled selected>- Pilih Kendaraan -</option>
+                  <option>Pesawat</option>
+                  <option>Kapal laut</option>
+                  <option>Roda empat</option>
+                  <option>Kendaraan umum</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <!-- Nama Penanda Tangan ST -->
+          <div class="form-group row">
+            <label for="namaPenandatanganst" class="col-sm-6 col-form-label">Nama Penanda Tangan ST<span class="wajib"> *</span></label>
+            <div class="col-sm-12">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <select class="form-control" name="namaPenandatangan" id="namaPenandatangan" required>
+                  <option value="" disabled selected>- Pilih Penanda Tangan Surat Tugas -</option>
+                  <option>Bagus Heri Purnomo, S.Si., Apt</option>
+                  <option>Larasati Setyaningtyas, S.Farm., Apt</option>
+                  <option>Dyah Ayu Novi Hapsari, S.Farm., Apt</option>
+                  <option>Ruth Deseyanti Purba, S.Si., Apt</option>
+                  <option>Irdiansyah, SH</option>
+                  <option>Paniyati, S.Farm., Apt</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <!-- Jabatan Penanda Tangan ST -->
+          <div class="form-group row">
+            <label for="jabatanPenandatanganst" class="col-sm-6 col-form-label">Jabatan Penanda Tangan ST<span class="wajib"> *</span></label>
+            <div class="col-sm-12">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <select class="form-control" name="jabatanPenandatangan" id="jabatanPenandatangan" required>
+                      <option value="" disabled selected>- Pilih Jabatan Penanda Tangan Surat Tugas -</option>
+                      <option>Kepala Balai POM di Batam</option>
+                      <option>PLH Kepala Balai POM di Batam Koordinator Substansi Informasi dan Komunikasi</option>
+                      <option>PLH Kepala Balai POM di Batam Koordinatitor Substansi Pengujian</option>
+                      <option>PLH Kepala Balai POM di Batam Koordinator Substansi Pemeriksaan</option>
+                      <option>PLH Kepala Balai POM di Batam Koordinator Substansi Penindakan</option>
+                      <option>PLH Kepala Balai POM di Batam Kepala SUB Bagian Tata Usaha</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+        </div> 
+
+
+        <div class="row">
+    <!-- left column -->
+    <div class="col-xs-12">
+      <!-- general form elements -->
+
+    <div class="box box-primary">
+      <div class="box-header with-border">
+
+      <hr>
+      <h4>Petugas</h4>
+      <hr>
+
+        <section class="content">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="box">
+
+              <!-- Petugas -->
+              <div class="form-group row">
+                <label for="pilihPetugas" class="col-sm-3 col-form-label">Pilih Petugas<span class="wajib"> *</span></label>
+                <div class="col-md-12">
+                  <select class="category related-post form-control" name="idPetugas[]" id= "idPetugas[]" multiple="multiple" data-placeholder="Pilih Petugas" style="width: 100%;" required>
+                    <?php
+                    foreach ($petugas as $pegawai) {
+                      echo "<option value=".$pegawai->idPegawai.">".$pegawai->nama."</option>";
+                    }
+                    ?>
+
+                  </select>
+                </div>
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <style>
+      th.dt-center, td.dt-center { text-align: center; }
+    </style>
+
+        <div class="box-footer">
+         <button type="submit" value="submit" onclick="return chk_date()" class="btn btn-info"><i class="fa fa-print"></i> Save Document</button>
+         <button type="reset"  value ="reset" class="btn btn-danger"><i class="fa fa-refresh" aria-hidden="true"></i> Reset Form</button>
+       </div>
+     </form>
+
+   </div>
+
+
+
+
+
+ </div>
+</div>
+<style>
+  .select2-container--default .select2-selection--multiple .select2-selection__choice {
+    background-color: #ca2e2e;
+    border: 1px solid #aaa;
+    border-radius: 4px;
+    box-sizing: border-box;
+    display: inline-block;
+    margin-left: 5px;
+    margin-top: 5px;
+    padding: 0;
+    padding-left: 20px;
+    position: relative;
+    max-width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    vertical-align: bottom;
+    white-space: nowrap;
+    font-size: 15px;
+  }
+</style>
+
+<!-- /.row -->
+</section>
+<!-- /.content -->
---
+++ application/views/admin/surat_tugas/surattugas_infokom.php
@@ -0,0 +1,327 @@
+<section class="content-header">
+ <h1>
+   Surat Tugas Substansi Infokom
+ </h1>
+ <ol class="breadcrumb">
+  <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+  <li><a href="#">Surat Tugas Substansi Infokom</a></li>
+</ol>
+</section>
+<!-- Main content -->
+<section class="content">
+
+  <div class="box">
+    <div class="box-header with-border">
+       <h4>Hai <b>Admin..</b> </h4>
+      <div class="box-tools pull-right">
+        <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
+        <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
+      </div>
+    </div>
+  </div>
+
+<div class="row">
+  <!-- left column -->
+  <div class="col-md-12">
+    <!-- general form elements -->
+
+    <div class="box box-primary">
+      <div class="box-header with-border">
+
+        <h3 class="box-title">Daftar Surat Tugas</h3>
+        <div class="pull-right">
+          <ul>
+            <a class= "btn btn-primary" href="<?php echo base_url('admin/surat_tugas/surattugas_infokom/tambah_surat')?>">
+              <i class="fa fa-plus"></i>&nbsp; Tambah Data 
+            </a> </span>
+          </ul>
+        </div>
+
+        <section class="content">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="box">
+
+                <!-- /.box-header -->
+                <div class="box-body">
+                  <table id="tbl" class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th class="dt-center">No Surat Tugas</th>
+                        <th class="dt-center">Tgl Surat Tugas</th>
+                        <th class="dt-center">Maksud & Tujuan</th>
+                        <th class="dt-center">Kota</th>
+                        <th class="dt-center">Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php
+                      if(isset($surattugas)){
+                       foreach ($surattugas as $row){
+
+                        echo "<tr>";
+                        echo "<td class='dt-center'>".$row->noSuratTugas."</td>";
+                        echo "<td class='dt-center'>".$row->tglSurat."</td>";
+                        echo "<td class='dt-center'>".$row->maksud."</td>";
+                        echo "<td class='dt-center'>".$row->kota."</td>";
+                        echo "<td class='dt-center'>"?>
+                        <a href="#" class="btn btn-success btn-sm" data-tooltip="tooltip" title="Edit" id="editSur"
+                        data-idsurat="<?=$row->idSurat ?>" data-nosurat="<?=$row->noSuratTugas ?>"  data-tglsurat="<?=$row->tglSurat ?>" data-maksud="<?=$row->maksud ?>" data-kota="<?=$row->kota ?>" data-anggaran="<?=$row->idAnggaran ?>" data-tglmulai="<?=$row->tglMulai ?>" data-bebanbiaya="<?=$row->bebanBiaya ?>" data-tglselesai="<?=$row->tglSelesai ?>" data-kendaraan="<?=$row->kendaraan ?>" data-namapenandatangan="<?=$row->namaPenandatangan ?>" data-jabatanpenandatangan="<?=$row->jabatanPenandatangan ?>"  data-toggle="modal" data-target="#editSurat">
+                          <i class="fa fa-edit"></i></a>
+
+                         <a href="#" data-tooltip="tooltip" title="Hapus" class="btn btn-danger btn-sm" id="hapusSur" data-id="<?=$row->idSurat ?>" data-toggle="modal" data-target="#hapusSurat">
+                          <i class="fa fa-trash"></i></a>
+
+                         <a href="#" class="btn btn-info btn-sm" data-tooltip="tooltip" title="Print" id="printSur" data-toggle="modal" data-target="#printSurat" data-id="<?=$row->idSurat ?>">
+                          <i class="fa fa-print"></i></a>
+                            
+                        </td>
+
+                        <?php
+                      }
+                    }else{
+                      echo "no record found";
+                    }
+                    ?>
+                  </tbody>
+									</table>
+								</div>
+								<!-- /.box-body -->
+							</div>
+							<!-- /.box -->
+						</div>
+						<!-- /.col -->
+					</div>
+					<!-- /.row -->
+				</section>
+				<!-- /.content -->
+			</div>
+			<style>
+			th.dt-center, td.dt-center { text-align: center; }
+		</style>
+
+	</div>
+	<!-- /.box-header -->
+	<!-- form start -->
+
+
+</div>
+</div>
+
+</div>
+<!-- /.box -->
+</div>
+
+</div>
+<!-- /.row -->
+
+<!-- Edit Surat Tugas -->
+<div id= "editSurat" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title"><i class="icon fa fa-edit"></i>  Edit Surat Tugas</h4>
+			</div>
+			<div class="modal-body" id=editData >
+				<form action="<?= base_url('admin/surat_tugas/surat_tugas/ubah_surat')?>" method="post">
+					<div class="box box-success">
+						<div class="box-body">
+
+							<div class="form-group">
+								<input type="hidden" class="form-control" name="idSurat" id="idSurat" >
+							</div>
+
+							<div class="form-group">
+								<label for="noEdit">No. Surat Tugas </label> <small class="text-danger">*</small>
+								<input type="text" class="form-control" name="noSurat" id="noSurat" >
+							</div>
+
+							<div class="form-group">
+								<label for="noEdit">Tanggal Surat </label> <small class="text-danger">*</small>
+								<input class="form-control" type="date" name ="tglSurat" id="tglSurat" onclick="return chk_date()" >
+							</div>
+
+              <div class="form-group">
+                <label for="noEdit">Kota </label> <small class="text-danger">*</small>
+                <select class="form-control" name="kota" id="kota" >
+                  <option>Kota Batam</option>
+                  <option>Kota Tanjung Pinang</option>
+                  <option>Kabupaten Bintan</option>
+                  <option>Kabupaten Karimun</option>
+                  <option>Kabupaten Anambas</option>
+                  <option>Kabupaten Lingga</option>
+                  <option>Kabupaten Natuna</option>
+                </select>
+              </div>
+
+              <div class="form-group">
+                <label for="noEdit">Anggaran </label> <small class="text-danger">*</small>
+                <select class="form-control" name="idAnggaran" id="idAnggaran" >
+                <?php
+                foreach ($anggaran as $anggaran) {
+                  echo "<option value=".$anggaran->idAnggaran.">".$anggaran->namaAnggaran."</option>";
+                }
+                ?>
+                </select>
+              </div>
+
+              <div class="form-group">
+                <label for="noEdit">Maksud </label> <small class="text-danger">*</small>
+                <input type="text" class="form-control" name="maksud" id="maksud" >
+              </div>
+
+              <div class="form-group">
+                <label for="noEdit">Tanggal Mulai Tugas </label> <small class="text-danger">*</small>
+                <input class="form-control" type="date" name ="tglMulai" id="tglMulai"  onclick="return chk_date()" >
+              </div>
+
+               <div class="form-group">
+                <label for="noEdit">Tanggal Selesai Tugas </label> <small class="text-danger">*</small>
+                <input class="form-control" type="date" name ="tglSelesai" id="tglSelesai" onclick="return chk_date()" >
+              </div>
+
+              <div class="form-group">
+                <label for="noEdit">Kendaraan </label> <small class="text-danger">*</small>
+                <select class="form-control" name="kendaraan" id="kendaraan" >
+                <option>Pesawat</option>
+                  <option>Kapal laut</option>
+                  <option>Roda empat</option>
+                  <option>Kendaraan umum</option>
+                  </select>
+              </div>
+
+              <div class="form-group">
+                <label for="noEdit">Nama Penanda Tangan ST </label> <small class="text-danger">*</small>
+                <select class="form-control" name="namaPenandatangan" id="namaPenandatangan" >
+                  <option>Bagus Heri Purnomo, S.Si., Apt</option>
+                  <option>Larasati Setyaningtyas, S.Farm., Apt</option>
+                  <option>Dyah Ayu Novi Hapsari, S.Farm., Apt</option>
+                  <option>Ruth Deseyanti Purba, S.Si., Apt</option>
+                  <option>Irdiansyah, SH</option>
+                  <option>Paniyati, S.Farm., Apt</option>
+                </select>
+              </div>
+
+              <div class="form-group">
+                <label for="noEdit">Jabatan Penanda Tangan ST </label> <small class="text-danger">*</small>
+                <select class="form-control" name="jabatanPenandatangan" id="jabatanPenandatangan"   >
+                <option>Kepala Balai POM di Batam</option>
+                      <option>PLH Kepala Balai POM di Batam Koordinator Substansi Informasi dan Komunikasi</option>
+                      <option>PLH Kepala Balai POM di Batam Koordinatitor Substansi Pengujian</option>
+                      <option>PLH Kepala Balai POM di Batam Koordinator Substansi Pemeriksaan</option>
+                      <option>PLH Kepala Balai POM di Batam Koordinator Substansi Penindakan</option>
+                      <option>PLH Kepala Balai POM di Batam Kepala SUB Bagian Tata Usaha</option>
+                </select>
+              </div>
+
+
+						</div><!-- /.box-body -->                        
+						<div class="modal-footer">
+							<button type="reset" class="btn btn-default pull-left" data-dismiss="modal"><i class="fa fa-times"></i> Tutup</button>
+							<button type="submit" name="update" class="btn btn-success"><i class="fa fa-edit"></i> Update</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="<?php echo base_url();?>assets/vendor/jquery/jquery-1.10.0.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$(document).on("click","#editSur",function(){
+		var idS = $(this).data('idsurat');
+		var noS = $(this).data('nosurat');
+		var tglS = $(this).data('tglsurat');
+		var maksudS = $(this).data('maksud');
+		var kotaS = $(this).data('kota');
+    var anggaranS = $(this).data('anggaran');
+    var tglmulaiS = $(this).data('tglmulai');
+    var tglselesaiS = $(this).data('tglselesai');
+    var kendaraanS = $(this).data('kendaraan');
+    var namaS = $(this).data('namapenandatangan');
+    var jabatanS = $(this).data('jabatanpenandatangan');
+
+		$("#editData #idSurat").val(idS);
+		$("#editData #noSurat").val(noS);
+		$("#editData #tglSurat").val(tglS);
+		$("#editData #maksud").val(maksudS);
+		$("#editData #kota").val(kotaS);
+    $("#editData #idAnggaran").val(anggaranS);
+    $("#editData #tglMulai").val(tglmulaiS);
+    $("#editData #tglSelesai").val(tglselesaiS);
+    $("#editData #kendaraan").val(kendaraanS);
+    $("#editData #namaPenandatangan").val(namaS);
+    $("#editData #jabatanPenandatangan").val(jabatanS);
+	});
+  </script>
+ <!-- Hapus Surat -->
+ <div id= "hapusSurat" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"><i class="icon fa fa-ban"></i> ALert !</h4>
+                </div>
+                <div class="modal-body" id=hapusData>
+                    <form role="form" method="post" action="<?= base_url('admin/surat_tugas/surat_tugas/hapus_surat') ?>">
+                        <div class="box-body">
+                            <div class="form-group" style="text-align:center">Anda yakin akan menghapus Data Pegawai ini ?</label>
+                                <input type="hidden" id="idSurat" name="idSurat">
+                              
+                            </div>                        
+                        </div><!-- /.box-body -->
+                        <div class="modal-footer">
+                            <button type="reset" class="btn btn-default pull-left" data-dismiss="modal"><i class="fa fa-times"></i> Tutup</button>
+                            <button type="submit" class="btn btn-danger" name="delete"><i class="fa fa-check"></i> Hapus</button>
+                        </div>
+                    </form>             
+                        <script src="<?php echo base_url();?>assets/js/jquery-1.10.0.min.js" type="text/javascript"></script>
+                        <script type="text/javascript" >
+                            $(document).on("click","#hapusSur",function(){
+                            var id = $(this).data('id');
+
+                            $("#hapusData #idSurat").val(id);
+                            });
+                        </script>
+                </div>
+                
+  </div>
+      </div>
+    </div>
+  </div>  
+
+<!-- Print Surat Tugas -->
+<div id= "printSurat" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title"><i class="icon fa fa-ban"></i> ALert !</h4>
+      </div>
+      <div class="modal-body" id=panelSur>
+        <form role="form" method="post" action="<?= base_url('admin/surat_tugas/surat_tugas/print')?>">
+          <div class="box-body">
+            <div class="form-group" style="text-align:center">Anda Akan Mencetak Surat Tugas</label>
+              <input type="hidden" id="idSurat" name="idSurat">
+
+            </div>                        
+          </div><!-- /.box-body -->
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-default pull-left" data-dismiss="modal"><i class="fa fa-times"></i> Tutup</button>
+            <button type="submit" class="btn btn-success" name="delete"><i class="fa fa-print"></i> Cetak</button>
+          </div>
+        </form>             
+        <script src="<?php echo base_url();?>assets/js/jquery-1.10.0.min.js" type="text/javascript"></script>
+        <script type="text/javascript" >
+          $(document).on("click","#printSur",function(){
+          var id = $(this).data('id');
+
+          $("#panelSur #idSurat").val(id);
+          });
+        </script>
+      </div>
+
+    </div>
+  </div>
+</div>
