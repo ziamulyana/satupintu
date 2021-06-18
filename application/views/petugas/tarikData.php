4a576f6da11cbcb6df250b17e26c97db668d2dfd
endahnasution <79795077+endahnasution@users.noreply.github.com>
2021-06-18 16:13:17 +0700

    update data

--- application/views/petugas/tarikData.php
+++ application/views/petugas/tarikData.php
@@ -39,13 +39,27 @@
                   <!-- no surat -->
                   <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-align-left"></i></span>
-                    <select class="form-control category" name="suratTugas" id="suratTugas" style="width: 100%;">
-                      <?php
-                      foreach ($surat_tugas as $surat) {
-                        echo "<option value=".$surat->idSurat.">".$surat->noSuratTugas."</option>";
-                      }
-                      ?>
-                      <option selected="selected">- Pilih Surat Tugas -</option>
+                    <select class="form-control category" name="kategori" id="kategori" style="width: 100%;">
+                    <option value="Apotek">Apotek</option>
+                    <option value="Toko Obat">Toko Obat</option>
+                    <option value="Rumah Sakit">Rumah Sakit</option>
+                    <option value="Puskesmas">Puskesmas</option>
+                    <option value="PBF">PBF</option>
+                    <option value="Klinik">Klinik</option>
+                    <option value="IFK">IFK</option>
+                    <option value="Toko Kosmetik">Toko Kosmetik</option>
+                    <option value="Pemilik Notifikasi">Pemilik Notifikasi</option>
+                    <option value="Konter Kosmetik">Konter Kosmetik</option>
+                    <option value="Klinik Kecantikan/Salon/Spa">Klinik Kecantikan/Salon/Spa</option>
+                    <option value="Industri Kosmetik Golongan B">Industri Kosmetik Golongan B</option>
+                    <option value="Industri Kosmetik Golongan A">Industri Kosmetik Golongan A</option>
+                    <option value="Importir Kosmetik">Importir Kosmetik</option>
+                    <option value="Distributor Kosmetik">Distributor Kosmetik</option>
+                    <option value="Agen/Stokis MLM">Agen/Stokis MLM</option>
+                    <option value="Retail Suplemen Kesehatan">Retail Suplemen Kesehatan</option>
+                    <option value="Retail Obat Tradisional">Retail Obat Tradisional</option>
+                    <option value="Importir Suplemen Kesehatan">Retail Obat Tradisional</option>
+                      <option selected="selected">- Pilih Jenis Sarana -</option>
                     </select>
                   </div>
                 </div>
@@ -58,17 +72,37 @@
                   <!-- no surat -->
                   <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-align-left"></i></span>
-                    <select class="form-control category" name="suratTugas" id="suratTugas" style="width: 100%;">
-                      <?php
-                      foreach ($surat_tugas as $surat) {
-                        echo "<option value=".$surat->idSurat.">".$surat->noSuratTugas."</option>";
-                      }
-                      ?>
-                      <option selected="selected">- Pilih Surat Tugas -</option>
+                    <select class="form-control category" name="komoditi" id="komoditi" style="width: 100%;">
+                    <option value="Obat">Obat</option>
+                    <option value="Kosmetik">Kosmetik</option>
+                    <option value="Obat Tradisional">Obat Tradisional</option>
+                    <option value="Suplemen Kesehatan">Suplemen Kesehatan</option>
+                    <option value="Pangan">Pangan</option>
+                    <option value="Semua">Semua</option>
+                      <option selected="selected">- Pilih Jenis Komoditi -</option>
                     </select>
                   </div>
                 </div>
               </div>
+
+
+              <!-- Status -->
+              <div class="form-group row">
+                <label for="noSurat" class="col-sm-6 col-form-label">Status Hasil Pemeriksaan<span class="wajib"> *</span></label>
+                <div class="col-sm-12">
+                  <!-- no surat -->
+                  <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-align-left"></i></span>
+                    <select class="form-control " name="status" id="status" style="width: 100%;">
+                    <option value="1">MK</option>
+                    <option value="0">TMK</option>
+                    <option value="Semua">Semua</option>
+                      <option selected="selected">- Pilih Status Hasil Pemeriksaan -</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
 
 
             </div>
@@ -99,6 +133,7 @@
                 </div>
               </div>
 
+                
 
              
 
@@ -146,5 +181,7 @@
     border-bottom: none;
     font-size: 16px;
   }
+
+  
 </style>
 
