516dc017ae9541e4b97f0cf581752169cc437d0c
server <serverbpom21@gmail.com>
2021-07-03 13:40:34 +0800

    2

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -13,7 +13,7 @@
           <div class="inner">
             <h3><?php echo $this->db->where('timeline >', 7)->where('status =', 0)->from("view_admin")->count_all_results(); ?></h3>
 
-            <p>AKTIFa</p>
+            <p>AKTIF</p>
           </div>
           <div class="icon">
             <i class="small fa fa-check-square-o"  style="font-size:70px;"></i>
